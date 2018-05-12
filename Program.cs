4fc89c956ed3a8fd945b0f2b3072dca6d42aa865
Garrett <garrettgucci@gmail.com>
2018-05-12 13:34:33 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Fundamentals_I
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 1; i <= 255; i++)
+            {
+                Console.WriteLine(i);
+            }
+
+            for (int k = 1; k <= 100; k++)
+            {
+                if (k % 3 == 0 || k % 5 == 0){
+                    if (k % 3 == 0 && k % 5 == 0){
+                    }
+                    else {
+                        Console.WriteLine(k);
+                    }
+                }
+            }
+
+            for (int j = 1; j <= 100; j++)
+            {
+                if (j % 3 == 0 && j % 5 == 0)
+                {
+                    Console.WriteLine("FizzBuzz" + " - " + j);
+                }
+                
+                else if (j % 3 == 0 && j % 5 != 0)
+                {
+                    Console.WriteLine("Fizz" + " - " + j);
+                }
+                else if (j % 3 != 0 && j % 5 == 0)
+                {
+                    Console.WriteLine("Buzz" + " - " + j);
+                }
+                
+            } 
+        }
+    
+    }
+}
